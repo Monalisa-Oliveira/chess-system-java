73fe4a13f40e722a159f5600f5f83997aec96649
Monalisa-Oliveira <moninhaoliveira14@gmail.com>
2021-08-07 22:28:07 -0300

    Movendo peças

--- src/aplicacao/Main.java
+++ src/aplicacao/Main.java
@@ -1,14 +1,28 @@
 package aplicacao;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		ChessMatch match = new ChessMatch();
 		
-		UI.printBoard(match.getPieces());
+		while(true) {
+			UI.printBoard(match.getPieces());
+			System.out.println();
+			System.out.println("Posição de origem: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			System.out.println("Posição de destino: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			ChessPiece capturedPiece = match.performChessMovie(source, target);
 
+		}
+		
 	}
 
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import borderGame.Board;
+import borderGame.Piece;
+import borderGame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -23,6 +25,26 @@ public class ChessMatch {
 			return mat;
 	}
 	
+	public ChessPiece performChessMovie(ChessPosition sourcePosition, ChessPosition targetPosition) {
+		Position source = sourcePosition.toPosition();
+		Position target = targetPosition.toPosition();
+		validateSourcePosition(source);
+		Piece capturedPiece = makeMovie(source, target);
+		return (ChessPiece)capturedPiece;
+	}
+	private Piece makeMovie(Position source, Position target) {
+		Piece p = board.removePiece(source);
+		Piece capturedPiece = board.removePiece(target);
+		board.placePiece(p, target);
+		return capturedPiece;
+	}
+
+	private void validateSourcePosition(Position position) {
+		if(!board.thereIsAPiece(position)) {
+			throw new ChessException("Não há peça na posição!");	
+		}
+	}
+
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
